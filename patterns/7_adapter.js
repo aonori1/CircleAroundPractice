36c2090497f3f3036b5ca6d63f002d5ef571ea23
aonori1 <acount.toroku@gmail.com>
2020-06-01 15:45:51 +0900

    adapterの学習

--- patterns/7_adapter.js
+++ patterns/7_adapter.js
@@ -60,7 +60,7 @@ let messageCreator = new MessageCreator;
 {
   let clock = new Clock(new Date(2020, 5, 1, 2));
   let adapter = new ClockMessageAdapter(clock);
-  
+
   messageCreator.message(adapter);
 }
 
@@ -83,16 +83,65 @@ class TimeByString {
   }
 }
 
+class TimeMessageAdapter {
+  #timeByString
+  constructor(timeByString) {
+    this.#timeByString = timeByString;
+  }
+
+  isMorning() {
+    let time = this.#timeByString.getString();
+    return time === '朝';
+  }
+
+  isAfterNoon() {
+    let time = this.#timeByString.getString();
+    return time === '昼';
+  }
+
+  isNight() {
+    let time = this.#timeByString.getString();
+    return time === '晩';
+  }
+}
+
+{
+  let timeByString = new TimeByString('朝');
+  let adapter = new TimeMessageAdapter(timeByString);
+
+  messageCreator.message(adapter);
+}
+{
+  let timeByString = new TimeByString('晩');
+  let adapter = new TimeMessageAdapter(timeByString);
+
+  messageCreator.message(adapter);
+}
+
 
 //////////////////////////////////////////////////////////////
 // Q2. Adapterパターンのクラスの構造は、Decoratorパターンとよく似ています。
 // どういう点が異なるのか、利用シーンや目的に違いがあるのかを考察しましょう。
 
+// A2. Decoratorは元のクラスを上書きすることで何度も別の機能を実装していた．
+// アルゴリズムが違うパターンの物を実装する際に用いる．
 
+// Adapterは間に互換性のあるクラスを挟むことで，
+// 当てはめたいクラス（Clock,TimeByString）ロジックを
+// 元のクラス（MessageCreator）に利用できる形に変換していた．
+// 基本アルゴリズムは同じだがデータなどタネとなる元が違う時に用いる．
 
 //////////////////////////////////////////////////////////////
 // Q3. Adapterパターンのクラスの構造は、Strategyパターンとほぼ同じです。
 // どういう点が異なるのか、利用シーンや目的に違いがあるのかを考察しましょう。
+
+// A3. 最終動作が変わらず，ロジックが外にある点ではどちらも同じ．
+
+// Strategyは出力の変更を容易にするために，ロジックを外に出しメンテナンスを容易にした．
+
+// Adapterは出力は一緒だが入力されるデータが違うため，
+// 別で出力にデータが合う様にロジックを組み直している．
+
 
 
 
